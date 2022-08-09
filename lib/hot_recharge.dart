2f08d764952e0beb4f8b91a430e222d8c7725819
iamngoni <imngonii@gmail.com>
2022-08-09 14:36:01 +0200

    fix: switch to relative imports & added linter rules

--- lib/hot_recharge.dart
+++ lib/hot_recharge.dart
@@ -1,6 +1,5 @@
 library hotrecharge;
 
-export 'package:hot_recharge/src/models/index.dart';
-export 'package:hot_recharge/src/utils.dart';
-
 export 'src/hot_recharge.dart';
+export 'src/models/index.dart';
+export 'src/utils.dart';
