a5534568e5206e636cd2a9961f28683a6e52e71c
iamngoni <imngonii@gmail.com>
2022-08-09 14:32:35 +0200

    fix: update all dependencies 1. replaced http with dio 2. updated uuid and logger 3. added flutter_lints dev dependency

--- lib/hot_recharge.dart
+++ lib/hot_recharge.dart
@@ -1,5 +1,6 @@
 library hotrecharge;
 
-export 'package:hot_recharge/src/hot_recharge.dart';
-export 'package:hot_recharge/src/utils.dart';
 export 'package:hot_recharge/src/models/index.dart';
+export 'package:hot_recharge/src/utils.dart';
+
+export 'src/hot_recharge.dart';
